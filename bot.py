28d5ed0698070f98f87410849f8fa51d65b3d323
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2016-04-27 16:54:25 +0500

    Добавил Procfile

--- bot.py
+++ bot.py
@@ -6,31 +6,29 @@ import sys
 import os
 from apscheduler.schedulers.blocking import BlockingScheduler
 from twitterbot_utils import Twibot, get_maximum_tweets
-from twitterbot_utils.TwiUtils import remove_mention
-from morpher import Morpher
-from celeb import search
+from twitterbot_utils.TwiUtils import remove_mentions
+from celeb.morpher import Morpher
+from celeb.celeb import search
 
 __author__ = "@strizhechenko"
 
 SCHED = BlockingScheduler()
 BOT = Twibot()
-READER = Twibot(username=os.environ.get('reader_name'))
 MORPHY = Morpher()
-TIMEOUT = int(os.environ.get('timeout', 30))
-TEMPLATE = unicode(os.environ.get('template', u''), 'utf-8')
+TIMEOUT = int(os.environ.get('timeout', 1))
 TWEET_GRAB = int(os.environ.get('tweet_grab', 3))
-TWEETS_PER_TICK = int(os.environ.get('tweets_per_tick', 2))
-POSTED = get_maximum_tweets(BOT.api.home_timeline)
+print 'fetch answered'
 ANSWERED = get_maximum_tweets(BOT.api.mentions_timeline)
+print 'fetched answered'
 
 @SCHED.scheduled_job('interval', minutes=TIMEOUT)
 def do_tweets():
     """ периодические генерация и постинг твитов """
     print 'New tick'
     tweets = BOT.api.mentions_timeline(count=TWEET_GRAB)
-    tweets = [tweet for tweet in tweets if tweet not in ANSWERED]
+    tweets = [tweet.text for tweet in tweets if tweet.text not in ANSWERED]
     for request in tweets:
-        name = remove_mention(request)
+        name = remove_mentions(request)
         answer = search(name)
         BOT.tweet(answer)
         ANSWERED.append(request)
